3b262f98b9e0413983714ac5de9c2dcd833931fd
cjj-sal <cjj_sal@163.com>
2020-10-19 23:20:55 +0800

    test

--- app/src/main/java/com/cjj/sal/MainActivity.java
+++ app/src/main/java/com/cjj/sal/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 你好
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO dss
+        //
     }
 }
